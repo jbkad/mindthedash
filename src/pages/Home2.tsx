973a366ab6b080f3aa17dcf78e659782cd79f9ed
jbkad <jbkadibu@gmail.com>
2025-11-06 22:47:19 +0000

    feat (home): img added

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -1,7 +1,8 @@
 const Home2: React.FC = () => {
     return (
-        <div className='flex mt-5 mb-2.5 w-full justify-center text-center'>
-            This site is currently going through some changes, please check back soon!
+        <div className='flex flex-col mt-5 mb-2.5 w-full justify-center text-center'>
+            <img src={require('../assets/train.gif')} alt='Train on a track' />
+            <p className='mt-5'>This site is currently going through some changes, please check back soon!</p>
         </div>
     )
 }
